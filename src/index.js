f32e6bd82d2d3f2f887f0e87398db8fd03ae1db7
AlinaS13 <soulofangel13@gmail.com>
2023-02-08 01:27:01 +0200

    add hw-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+import { fetchCountries, countryList, countryInfo } from './fetchCountries';
+
+const DEBOUNCE_DELAY = 400;
+
+const input = document.getElementById('search-box');
+console.log(input);
+
+const debounced = debounce(() => {
+  if (input.value.trim() === '') {
+    countryList.textContent = '';
+    countryInfo.textContent = '';
+    return;
+  }
+  fetchCountries(input.value.trim());
+}, DEBOUNCE_DELAY);
+
+input.addEventListener('input', debounced);
